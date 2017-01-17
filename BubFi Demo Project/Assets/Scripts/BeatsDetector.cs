723c3ead06ec5a66d513b0c74f12a3a7eed95817
bilal3387 <bilal3387@gmail.com>
2017-01-17 10:30:00 +0500

    BuFi Demo Project Added

---
+++ BubFi Demo Project/Assets/Scripts/BeatsDetector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BeatsDetector : MonoBehaviour
+{
+	public Image image;
+	public BubFiAudioVisualizer visualizer;
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (image != null && visualizer != null) {
+			Color c = visualizer.color;
+			c.r *= 10f;
+			c.g *= 10f;
+			c.b *= 10f;
+
+			image.color = c;
+		} else
+			Debug.Log ("image or visualizer NULL");
+
+	}
+}
---
+++ BubFi Demo Project/Assets/Scripts/BubFiDemo.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubFiDemo : MonoBehaviour {
+
+	public void RED()
+	{
+		BubFiEffectsController.defaultController ().RED ();
+	}
+	
+	public void GREEN()
+	{
+		BubFiEffectsController.defaultController ().GREEN ();
+	}
+	
+	public void BLUE()
+	{
+		BubFiEffectsController.defaultController ().BLUE ();
+	}
+
+	public void flash()
+	{
+		BubFiEffectsController.defaultController ().FLASH (Color.red, 200);
+	}
+	
+	public void gradient()
+	{
+		Color[] colors = {Color.red, Color.green, Color.blue, Color.cyan, Color.magenta};
+		BubFiEffectsController.defaultController ().GRADIENT (colors, 15000);
+	}
+	
+	public void dim()
+	{
+		BubFiEffectsController.defaultController ().FADEIN(Color.red, 3000);
+	}
+}
